7200a746a0e4fcaa18e56547fbe93b86f59da429
Trinidad Padilla <tpadilla101@live.com>
2022-02-07 17:14:31 -0800

    Learned how to instantialize classes in Python3

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-#Practicing OOP concepts:
+#Concepts in OOP:
 
 #Practicing making an empty class:
 class Empty:
@@ -8,15 +8,30 @@ class User:
   pass
 
 #instantiating a new user from the User class:
-user1 = User()
-print(user1) #prints an empty user object with the address at <__main__.User object at 0x7f91b001bdc0>
+# user1 = User()
+# print(user1) #prints an empty user object with the address at <__main__.User object at 0x7f91b001bdc0>
 
 user2 = User()
 print(user2)
 
-# E.g. Define a class called Vehicle.  It should be completely empty/. After the class is defined, create two instances of Vehicle.  Save one to a variable called car, and another to a variable called boat
+# E.g. Define a class called Vehicle. After the class is defined, create two instances of Vehicle.  Save one to a variable called car, and another to a variable called boat
 
 class Vehicle:
-  pass
-car = Vehicle()
-boat = Vehicle()
+  def __init__(self, make, model, year):
+    self.make = make
+    self.model = model
+    self.year = year
+
+boat = Vehicle("Honda", "Civic", "2003")
+print(boat.model)
+
+#Instantiating a Class:
+#v = Vehicle("Honda", "Civic", 2017)
+
+#E.g.
+# Define the Comment class below:
+class Comment:
+    def __init__(self, username, text, likes=0):
+        self.username = username
+        self.text = text
+        self.likes = likes
