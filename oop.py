c0b8a9820775a5bc1df526b71395bbc98804a510
Trinidad Padilla <tpadilla101@live.com>
2022-02-07 16:41:32 -0800

    Working on OOP in Python

---
+++ oop.py
@@ -0,0 +1,22 @@
+#Practicing OOP concepts:
+
+#Practicing making an empty class:
+class Empty:
+  pass
+
+class User:
+  pass
+
+#instantiating a new user from the User class:
+user1 = User()
+print(user1) #prints an empty user object with the address at <__main__.User object at 0x7f91b001bdc0>
+
+user2 = User()
+print(user2)
+
+# E.g. Define a class called Vehicle.  It should be completely empty/. After the class is defined, create two instances of Vehicle.  Save one to a variable called car, and another to a variable called boat
+
+class Vehicle:
+  pass
+car = Vehicle()
+boat = Vehicle()
